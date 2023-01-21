372f81b8011cb5f813b3c9df35f5c35e5afd6f20
Ivo Limic <ivo@convertiv.com>
2023-01-21 18:45:32 +0100

    Refactoring php-cs-fixer config file.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,9 +3,7 @@
 // get a list of all files ending with ".php" in the entire project
 $finder = PhpCsFixer\Finder::create()->ignoreDotFiles(false)->in(__DIR__);
 
-$config = new PhpCsFixer\Config();
-
-return $config->setRules([
+return (new PhpCsFixer\Config())->setRules([
         '@PSR12' => true,
         // 'strict_param' => true,
         'array_syntax' => ['syntax' => 'short'],
